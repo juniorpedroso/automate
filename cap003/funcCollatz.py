dcf095efbf311d18c360311c24e845057f99b6ed
Junior Pedroso <sintasebemagora@gmail.com>
2022-05-28 21:45:37 -0300

    Update funcCollatz.py

--- cap003/funcCollatz.py
+++ cap003/funcCollatz.py
@@ -1,6 +1,3 @@
-from tkinter import N
-
-
 def collatz(number):
     numberValid = False
     try:
@@ -8,20 +5,28 @@ def collatz(number):
         numberValid = True
     except:
         print('Digite um número inteiro válido.')
+        res = number
     
-    print(f'Você digitou {number}')
     if numberValid:
-        if n % 2 == 0:
+        if (n % 2) == 0:
             res = n // 2
             print(res)
         else:
-            print(n)
+            # print(n)
             res = (n * 3) + 1
-
+            print(res)
+    if numberValid: 
+        return res
 
 print()
 n = input('Digite um número: ')
-collatz(n)
+
+while True:
+    n = collatz(n)
+    if n == 1:
+        break
+    elif type(n) != int:
+        n = input('Digite um número: ')
 
 
 

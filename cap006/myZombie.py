352b09a7475747e4d23944832a2b4820c3bee5f6
Junior Pedroso <sintasebemagora@gmail.com>
2022-12-30 20:41:03 -0300

    Update

--- cap006/myZombie.py
+++ cap006/myZombie.py
@@ -4,7 +4,7 @@ class MyZombie:
     def __init__(self, name):
         # All zombies must have a name:
         self.name = name
-
+    
     def turn(self, gameState):
         # gameState is a dict with info about the current state of the game.
         # You can choose to ignore it in your code.
@@ -29,15 +29,15 @@ class MyZombie:
             else:
                 break
 
-    zombies = (
-        zombiedice.examples.RandomCoinFlipZombie(name='Random'),
-        zombiedice.examples.RollsUntilInTheLeadZombie(name='Until Leading'),
-        zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 2 Shotguns', minShotguns=2),
-        zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 1 Shotgun', minShotguns=1),
-        MyZombie(name='My Zombie Bot'),
-        # Add any other zombie players here.
-    )
+zombies = (
+    zombiedice.examples.RandomCoinFlipZombie(name='Random'),
+    zombiedice.examples.RollsUntilInTheLeadZombie(name='Until Leading'),
+    zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 2 Shotguns', minShotguns=2),
+    zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 1 Shotgun', minShotguns=1),
+    MyZombie(name='My Zombie Bot'),
+    # Add any other zombie players here.
+)
 
-    # Uncomment one of the following lines to run in CLI or Web GUI mode:
-    # zombiedice.runTournament(zombies=zombies, numGames=1000)
-    zombiedice.runWebGui(zombies=zombies, numGames=1000)
+# Uncomment one of the following lines to run in CLI or Web GUI mode:
+# zombiedice.runTournament(zombies=zombies, numGames=1000)
+zombiedice.runWebGui(zombies=zombies, numGames=1000)
